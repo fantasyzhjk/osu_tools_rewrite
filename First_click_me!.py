c98a46e79874f9ded7248eb8af784750f60b48c5
Adsicmes <1483492332@qq.com>
2021-08-25 15:31:52 +0800

    修正oppadc的下载地址 如果github不能用换镜像站下载

--- First_click_me!.py
+++ First_click_me!.py
@@ -1,4 +1,7 @@
 import os
 
 os.system("pip install -r requirements.txt")
-os.system("python -m pip install git+https://github.com/The-CJ/oppadc.py.git#egg=oppadc")
+try:
+    os.system("python -m pip install git+https://github.com/The-CJ/oppadc.py.git#egg=oppadc")
+except:
+    os.system("python -m pip install git+https://hub.fastgit.org/The-CJ/oppadc.py.git#egg=oppadc")
